861ba9db512a1cd19f861cfe8ad07eed5f11c554
kingdomcoder <kingdomcoding@gmail.com>
2020-10-05 13:08:10 +0100

    First Rock Paper Scissors commit

--- src/kata.js
+++ src/kata.js
@@ -1,3 +1,9 @@
-let Person = function() {
-  return { greet: "Hello!" };
+function playRound(myMove, theirMove) {
+  if((myMove === "rock"  && theirMove === "scissors") || (myMove === "scissors" && theirMove === "paper") || (myMove === "paper" && theirMove === "rock"))
+    return "me";
+  
+  if(myMove === theirMove)
+    return "neither";
+
+  return "them";
 }
--- test/kata.spec.js
+++ test/kata.spec.js
@@ -1,11 +1,104 @@
-describe("Person", function () {
-  it("has a greeting", function() {
-	// arrange
-	let person = new Person();
-	// act
-	let actual = person.greet;
-	// assert
-	const expected = "Hello!";
-	expect(actual).toBe(expected);
-  });
+describe("Given that I have chosen rock", function () {
+	let myMove = "rock";
+
+	describe("When the opponent chooses scissors", function () {
+		let theirMove = "scissors";
+
+		it("Then I should win", function () {
+			
+			let winner = playRound(myMove, theirMove);
+
+			expect(winner).toBe("me");
+		});
+	});
+
+	describe("When the opponent chooses paper", function() {
+		let theirMove = "paper";
+
+		it("Then they should win", function() {
+			let winner = playRound(myMove, theirMove);
+
+			expect(winner).toBe("them");
+		});
+	});
+
+	describe("When the opponent chooses rock", function() {
+		let theirMove = "rock";
+
+		it("Then it should be a draw", function() {
+			let winner = playRound(myMove, theirMove);
+
+			expect(winner).toBe("neither");
+		})
+	})
+	
+});
+
+describe("Given that I have chosen scissors", function () {
+	let myMove = "scissors";
+
+	describe("When the opponent chooses rock", function () {
+		let theirMove = "rock";
+
+		it("Then they should win", function() {
+			let winner = playRound(myMove, theirMove);
+
+			expect(winner).toBe("them");
+		})
+	});
+
+	describe("When the opponent chooses paper", function() {
+		let theirMove = "paper";
+
+		it("Then I should win", function() {
+			let winner = playRound(myMove, theirMove);
+
+			expect(winner).toBe("me");
+		})
+	})
+
+	describe("When the opponent chooses scissors", function() {
+		let theirMove = "scissors";
+
+		it("Then it should be a draw", function() {
+			let winner = playRound(myMove, theirMove);
+
+			expect(winner).toBe("neither");
+		});
+	})
+});
+
+describe("Given that I have chosen paper", function () {
+	let myMove = "paper";
+
+	describe("When the opponent chooses scissors", function () {
+		let theirMove = "scissors";
+
+		it("Then they should win", function() {
+			let winner = playRound(myMove, theirMove);
+
+			expect(winner).toBe("them");
+		});
+	});
+	
+	describe("When the opponent chooses rock", function() {
+		let theirMove =  "rock";
+
+		it("Then I should win", function() {
+			let winner = playRound(myMove, theirMove);
+
+			expect(winner).toBe("me");
+		});
+	});	
+
+	describe('When the opponent chosses paper', function() {
+		let theirMove = "paper";
+
+		it("Then it should be a draw", function() {
+			let winner = playRound(myMove, theirMove);
+
+			expect(winner).toBe("neither");
+		})
+	})
+	
 });
